0396f79d4e242f57e380f97281af96bd2a80ffa1
Harishbm502 <149860285+Harishbm502@users.noreply.github.com>
2024-04-09 11:01:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,12 @@ import streamlit as st
 import os
 import time
 import glob
-import os
-
-
 from gtts import gTTS
 from googletrans import Translator
 
 try:
     os.mkdir("temp")
-except:
+except FileExistsError:
     pass
 
 st.title("Multilingual Text-to-Speech Converter")
@@ -23,36 +20,14 @@ in_lang = st.selectbox(
     "Select your input language",
     ("English", "Hindi", "Bengali", "Korean", "Chinese", "Japanese"),
 )
-if in_lang == "English":
-    input_language = "en"
-elif in_lang == "Hindi":
-    input_language = "hi"
-elif in_lang == "Bengali":
-    input_language = "bn"
-elif in_lang == "Korean":
-    input_language = "ko"
-elif in_lang == "Chinese":
-    input_language = "zh-cn"
-elif in_lang == "Japanese":
-    input_language = "ja"
+input_language_dict = {"English": "en", "Hindi": "hi", "Bengali": "bn", "Korean": "ko", "Chinese": "zh-cn", "Japanese": "ja"}
+input_language = input_language_dict.get(in_lang, "en")
 
 out_lang = st.selectbox(
     "Select your output language",
     ("English", "Hindi", "Bengali", "Korean", "Chinese", "Japanese"),
 )
-if out_lang == "English":
-    output_language = "en"
-elif out_lang == "Hindi":
-    output_language = "hi"
-elif out_lang == "Bengali":
-    output_language = "bn"
-elif out_lang == "Korean":
-    output_language = "ko"
-elif out_lang == "Chinese":
-    output_language = "zh-cn"
-elif out_lang == "Japanese":
-    output_language = "ja"
-
+output_language = input_language_dict.get(out_lang, "en")
 
 def text_to_speech(input_language, output_language, text):
     translation = translator.translate(text, src=input_language, dest=output_language)
@@ -64,7 +39,6 @@ def text_to_speech(input_language, output_language, text):
         my_file_name = "audio"
     tts.save(f"temp/{my_file_name}.mp3")
     return my_file_name, trans_text
-
 
 display_output_text = st.checkbox("Display output text")
 
@@ -79,7 +53,6 @@ if st.button("Convert"):
         st.markdown(f"## Output text:")
         st.write(f" {output_text}")
 
-
 def remove_files(n):
     mp3_files = glob.glob("temp/*mp3")
     if len(mp3_files) != 0:
@@ -89,6 +62,5 @@ def remove_files(n):
             if os.stat(f).st_mtime < now - n_days:
                 os.remove(f)
                 print("Deleted ", f)
-
 
 remove_files(7)
